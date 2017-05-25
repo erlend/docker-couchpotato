2d54a9f91b99fd713192e2bf680a57d487423e7d
Erlend Finvåg <erlend.finvag@gmail.com>
2017-05-25 03:42:22 +0200

    Merge data and config dirs

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
 
 mkdir -p /config
-mkdir -p /data
 
 cd /CouchPotatoServer
 touch /config/CouchPotato.cfg
 
-exec /usr/bin/python /CouchPotatoServer/CouchPotato.py --data_dir /data/ --config_file=/config/CouchPotato.cfg --console_log
+exec /usr/bin/python /CouchPotatoServer/CouchPotato.py --data_dir /config --config_file=/config/CouchPotato.cfg --console_log
